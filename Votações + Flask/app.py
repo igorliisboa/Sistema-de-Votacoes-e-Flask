bbd24cd0c89bb0f84017e3766669c1f358504096
Igor Lisboa <62958588+igorliisboa@users.noreply.github.com>
2021-02-27 19:42:45 -0300

    Update app.py

--- Votações + Flask/app.py
+++ Votações + Flask/app.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-@app.route("/sistema.html")
+@app.route("/sistema")
 def votacao():
     return render_template("sistema.html")
 
